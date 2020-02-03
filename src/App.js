ac3056045923dcc7a3d2aa6dfa5bec468f16d4fa
Mhayk Whandson <hi@mhayk.com>
2020-02-03 20:52:30 +0000

    Navbar and footer have been styled.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="grid-container">
-      <header className="navbar"></header>
+      <header className="navbar">
+        <div className="navbar__search">Search ...</div>
+        <div className="navbar__avatar">Your face</div>
+      </header>
       <aside className="sidebar"></aside>
       <main className="main"></main>
-      <footer className="footer"></footer>
+      <footer className="footer">
+        <div className="footer__copyright">&copy; 2020 Mhayk Whandson</div>
+        <div className="footer__signature">Made with grid-layout and flex-layout.</div>
+      </footer>
     </div>
   );
 }
